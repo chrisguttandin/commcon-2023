c3ba25e14827019895179be3bb948afa14fe0563
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-06-26 16:58:42 +0200

    fix: avoid unnecessary line breaks and overflows

--- src/app/slides/slide-eighteen/slide-eighteen.component.ts
+++ src/app/slides/slide-eighteen/slide-eighteen.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'cco-slide-eighteen',
+    styleUrls: ['./slide-eighteen.component.scss'],
     templateUrl: './slide-eighteen.component.html'
 })
 export class SlideEighteenComponent {}
